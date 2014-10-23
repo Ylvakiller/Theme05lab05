d972818596f08a333686ad2ff174a09ace08e4bc
Ylvakiller <rem.geuze@gmail.com>
2014-10-23 19:51:51 +0200

    Added description bb

--- src/lab5MotorBoats/MotorBoat.java
+++ src/lab5MotorBoats/MotorBoat.java
@@ -1,5 +1,24 @@
 package lab5MotorBoats;
 
+/*
+ * Description as found on bb:
+ * MotorBoats: 
+ * Consider a class MotorBoat that represents motorboats. A motorboat has attributes for: 
+ * o the capacity of the fuel tank 
+ * o the maximum speed of the boat 
+ * o the current speed of the boat 
+ * 
+ * Add methods to the class to perform the following tasks: 
+ * o Set the capacity of the fuel tank of a MotorBoat object. 
+ * o Set the maximum speed of a MotorBoat object. 
+ * o Set the current speed of a MotorBoat object.  
+ * o Test whether two MotorBoat objects are equal (have the same maximum speed, same current speed and the same fuel tank capacity). 
+ * o Test whether two MotorBoat objects have the same maximum speed. 
+ * o Test whether two MotorBoat objects have the same fuel tank capacity. 
+ * o Test whether one MotorBoat object currently drives faster than another 
+ * o Test whether one MotorBoat object can drive faster than another (comparing the maximum speed)
+ * Write a driver (test) program that demonstrates each of the test method, with at least one true and one false case for each of the methods tested. 
+ */
 public class MotorBoat {
 
 }
