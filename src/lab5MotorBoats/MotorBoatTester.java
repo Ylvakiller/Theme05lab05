c875012772b26a49592ebd300b00acd00cfdc34e
Ylvakiller <rem.geuze@gmail.com>
2014-10-23 19:43:04 +0200

    Starting lab 5

---
+++ src/lab5MotorBoats/MotorBoatTester.java
@@ -0,0 +1,10 @@
+package lab5MotorBoats;
+
+public class MotorBoatTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/lab5TicTacToe/TicTacToeTester.java
@@ -0,0 +1,10 @@
+package lab5TicTacToe;
+
+public class TicTacToeTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
