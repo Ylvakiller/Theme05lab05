5b631e4631f54094c67ff8d9efc3753f043ffa20
Ylvakiller <rem.geuze@gmail.com>
2014-10-23 20:36:39 +0200

    started on the Tester

--- src/lab5MotorBoats/MotorBoatTester.java
+++ src/lab5MotorBoats/MotorBoatTester.java
@@ -1,7 +1,14 @@
 package lab5MotorBoats;
-
+/**
+ * Will test all the features of the motorboat class
+ * @author Ylva
+ *
+ */
 public class MotorBoatTester {
-
+	/**
+	 * Well what will a main method do... anyone any guess?
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 		
