ee587c9f639940d6ddd9c57bdf55ed91271ca159
Ylvakiller <rem.geuze@gmail.com>
2014-10-25 13:07:15 +0200

    made char array private

--- src/lab5TicTacToe/TicTacToe.java
+++ src/lab5TicTacToe/TicTacToe.java
@@ -5,5 +5,9 @@ package lab5TicTacToe;
  *
  */
 public class TicTacToe {
-
+	/**
+	 * This character array will hold the game field, it will be the size of the game
+	 * A value can be either 'A'(empty) 'O'(player sign) or 'X'(other player sign)
+	 */
+	private char[][] game;
 }
