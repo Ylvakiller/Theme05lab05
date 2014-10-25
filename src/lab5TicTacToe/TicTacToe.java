daf1d11ff0a472c21aa326f2ea101ba2087223eb
Ylvakiller <rem.geuze@gmail.com>
2014-10-25 13:22:48 +0200

    default constructor, emptyGame method and more
    Include the start of the tester

--- src/lab5TicTacToe/TicTacToe.java
+++ src/lab5TicTacToe/TicTacToe.java
@@ -10,4 +10,46 @@ public class TicTacToe {
 	 * A value can be either 'A'(empty) 'O'(player sign) or 'X'(other player sign)
 	 */
 	private char[][] game;
+	
+	/**
+	 * This integer counts the amount of moves, it is started at 0, every even move is a move made by the starting player (where 0 is said to be even)
+	 */
+	private int moveCount;
+	
+	/**
+	 * This char is either X or O depending on which user starts
+	 */
+	private char starter;
+	
+	/**
+	 * The size of the board
+	 */
+	private int gameSize;
+	
+	public TicTacToe(){
+		gameSize = 4;
+		game = new char[gameSize][gameSize];
+		emptyGame();
+		moveCount = 0;
+		starter = 'X';
+	}
+	
+	/**
+	 * I ran into problems with the way I initialized the game array,
+	 * Since the array size is usually really small we can just use a simple loop iteration to make the array hold values of 'A'
+	 */
+	private void emptyGame(){
+		int x = 0, y = 0;
+		while (x<gameSize){
+			while (y<gameSize){
+				game[x][y] = 'A';
+				y++;
+			}
+			x++;
+		}
+	}
+	
+	
+	
+	
 }
--- src/lab5TicTacToe/TicTacToeTester.java
+++ src/lab5TicTacToe/TicTacToeTester.java
@@ -62,7 +62,7 @@ public class TicTacToeTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		TicTacToe game1 = new TicTacToe();
 	}
 
 }
