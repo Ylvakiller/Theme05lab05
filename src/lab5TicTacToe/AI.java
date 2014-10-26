5c4e62d04a8761d00d58f54c222ea0d13553eb1e
Ylvakiller <rem.geuze@gmail.com>
2014-10-26 14:08:23 +0100

    Started with AI

---
+++ src/lab5TicTacToe/AI.java
@@ -0,0 +1,15 @@
+package lab5TicTacToe;
+
+/**
+ * This class will hold the player versus computer methods, it uses the same methods as the TicTacToe class only it has a AI layer
+ * @author Ylva
+ *
+ */
+public class AI extends TicTacToe{
+	/**
+	 * This character will be the character that the computer has to play with
+	 */
+	private char computer;
+	
+	
+}
